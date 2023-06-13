ae68714035619100ba31bd676899f96c7b0bb7fe
bryan-pham-124 <painfuljet8@gmail.com>
2023-06-13 18:17:42 -0400

    Adjustment to Text on XL Screens
    Made more headings scale up on XL screens.

--- src/components/text/H2.js
+++ src/components/text/H2.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const H2 = ({text, color ='black', textAlign='left'}) => {
   return (
-    <h2 className={`font-semibold text-${color} text-4xl md:text-6xl text-${textAlign} my-6 `}>{text}</h2>
+    <h2 className={`font-semibold text-${color} text-4xl md:text-6xl text-${textAlign} my-6 xl:text-8xl`}>{text}</h2>
   )
 }
 
--- src/components/text/MediumSubtitle.js
+++ src/components/text/MediumSubtitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MediumSubtitle = ({text, color ='black', textAlign='left'}) => {
   return (
-    <h2 className={`font-semibold text-${color} text-2xl md:text-4xl text-${textAlign} my-6 `}>{text}</h2>
+    <h2 className={`font-semibold text-${color} text-2xl md:text-4xl text-${textAlign} my-6 xl:text-6xl`}>{text}</h2>
   )
 }
 
